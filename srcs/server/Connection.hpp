e51a231eaf229940dc8c4ad52405ad80c63966a0
chlee <chlee@student.42seoul.kr>
2021-05-16 18:10:16 +0900

    add cstring header to find strlen

--- srcs/server/Connection.hpp
+++ srcs/server/Connection.hpp
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <cstring>
 #include "server/ServerComponent.hpp"
 #include "server/Request.hpp"
 #include "fdmanager/FileDiscriptorListener.hpp"
