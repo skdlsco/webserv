831eb4d82a68fe514d831ed5866971895e59502d
chlee <chlee@student.42seoul.kr>
2021-05-17 05:17:24 +0900

    add getLine() and some file I/O functions

---
+++ srcs/file/File.hpp
@@ -0,0 +1,48 @@
+#ifndef FILE_HPP
+# define FILE_HPP
+
+# define BUFFER_SIZE 1024
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <fcntl.h>
+#include "utils/String.hpp"
+
+class File
+{
+	enum State
+	{
+		CONTENT_LEFT, CONTENT_NOT_LEFT, DONE
+	};
+
+	private:
+		int mFD;
+		enum State mState;
+		std::string mFileName;
+		std::string mBuffer;
+		File();
+
+	public:
+		File(std::string const & fileName);
+		File(File const & copy);
+		File &operator=(File const & rhs);
+		virtual ~File();
+
+		void openFile();
+		void closeFile();
+		void readFile();
+		std::string getLine();
+
+		class FileException : public std::exception
+		{
+			private:
+				std::string mMessage;
+
+			public:
+				virtual ~FileException() throw();
+				FileException(std::string message) throw();
+				virtual const char* what() const throw();
+		};
+};
+
+#endif
