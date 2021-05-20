a8e3e12117b8b4ba0f2fedd636a8d1061124fef9
chlee <chlee@student.42seoul.kr>
2021-05-20 19:23:34 +0900

    remove variable

--- srcs/utils/Config.cpp
+++ srcs/utils/Config.cpp
@@ -2,14 +2,12 @@
 
 ServerConfig *web::getConfigMatchedWithHost(std::string const & host, std::vector<ServerConfig *> & configList)
 {
-	ServerConfig *defaultServer = web::getDefaultServerConfig(configList);
-
 	for (std::vector<ServerConfig *>::iterator iter = configList.begin(); iter != configList.end(); iter++)
 	{
 		if ((*iter)->getServerName() == host)
 			return (*iter);
 	}
-	return (defaultServer);
+	return (web::getDefaultServerConfig(configList));
 }
 
 ServerConfig *web::getDefaultServerConfig(std::vector<ServerConfig *> & configList)
