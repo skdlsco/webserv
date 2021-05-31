a51ff3386d1689d32ebf56846ed73dd6fe625cf4
chlee <chlee@student.42seoul.kr>
2021-05-31 23:18:19 +0900

    fix hyperlink

--- srcs/server/GETResponse.cpp
+++ srcs/server/GETResponse.cpp
@@ -148,7 +148,11 @@ std::string GETResponse::makeAutoIndexContent()
 	{
 		fileName = file->d_name;
 		filePath = locationURI + fileName;
-		autoIndexContent += "<a href=\"" + filePath + "\">" + fileName + "</a>";
+
+		if (file->d_name != "." && file->d_name != "..")
+			autoIndexContent += "<a href=\"" + filePath + "\">" + fileName + "</a>";
+		else
+			autoIndexContent += "<a href=\"" + fileName + "\">" + fileName + "</a>";
 		for (size_t idx = 0; idx < 70 - fileName.length(); idx++)
 		{
 			autoIndexContent += " ";
