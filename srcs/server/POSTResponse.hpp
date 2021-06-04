eb5b71821d193761aef70b5eae85d5e322ac5202
ina <ina@student.42seoul.kr>
2021-06-04 19:45:24 +0900

    fix POST PUT

--- srcs/server/POSTResponse.hpp
+++ srcs/server/POSTResponse.hpp
@@ -4,22 +4,24 @@
 #include "Response.hpp"
 #include "utils/Auth.hpp"
 #include "utils/String.hpp"
+#include "server/ErrorResponse.hpp"
 
 class POSTResponse : public Response
 {
 	private:
-		static const int BUFFER_SIZE = 1024;
+		static const int BUFFER_SIZE = 8192;
 		POSTResponse();
 
 		std::string mFileName;
 		std::string mBody;
 
+		void errorExcept();
 		void checkAuthorization();
 		void createFileName(std::string const & path);
 		void checkTarget();
 		void writeFile();
-		void appendResponseHeader(std::string &responseContent);
-		void appendResponseBody(std::string &responseContent);
+		void appendResponseHeader();
+		void appendResponseBody();
 	public:
 		static std::string const TAG;
 		POSTResponse(const ServerConfig * serverConfig, const LocationConfig * locationConfig);
@@ -27,7 +29,7 @@ class POSTResponse : public Response
 		POSTResponse(POSTResponse const & copy);
 		POSTResponse &operator=(POSTResponse const & rhs);
 
-		std::string *getResponse();
+		virtual void run();
 };
 
 #endif
